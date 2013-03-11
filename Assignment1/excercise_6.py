f83592e316ca7eb505063ff46d5f77c1ddc9f5ef
vuyani <g09m1782@campus.ru.ac.za>
2013-03-11 12:16:40 +0200

    UPLOADING excercise6

--- Assignment1/excercise_6.py
+++ Assignment1/excercise_6.py
@@ -3,13 +3,9 @@ population=raw_input("what is the population")
 capital=raw_input("is this city a Capital? yes or no")
 income=raw_input("what is the income")
 
-
 if capital=="yes":
 	if int(population) > 100000:
-		if int(income)>= 720000000:
-			print cityname, "is a metropoly"
-		elif int(income)< 720000000:
-			print cityname, "is not a metropoly because income is less than 720000000"
+		print cityname, "is a metropoly"
 	elif int(population) < 100000:
 		print cityname, "is not a metropoly because population is less than 100000 "
 if capital=="no":

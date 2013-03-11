9bb51cf79a465f00bc61e382243ecdbc4b1cc1b1
vuyani <g09m1782@campus.ru.ac.za>
2013-03-11 12:18:19 +0200

    UPLOADING excercise6

--- Assignment1/excercise_4.py
+++ Assignment1/excercise_4.py
@@ -1,10 +1,9 @@
 def y(m,x,b):
 	y= m * x + b
 	return y
-m=raw_input("enter m")#m value is 1
-b=raw_input("enter b")#b value is 0
+
 print "The results for the linear equation Y=mX+b with m=1 and b=0 are:","\n" "X","\t","Y"
 for i in range(1,11):
-	yint=y(m,i,b)
+	yint=y(1,i,0)
 	
 	print i,"\t",yint
